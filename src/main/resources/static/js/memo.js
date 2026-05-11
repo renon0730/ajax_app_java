76adaef2704a62f12dfa20eb1ec4e74d55c115a2
renon0730 <r.inoue@ibis-japan.co.jp>
2026-05-11 11:20:15 +0900

    コメント機能実装,js修正

--- src/main/resources/static/js/memo.js
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html xmlns:th="http://www.thymeleaf.org" lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <title>AjaxApp</title>
-  <link rel="stylesheet" type="text/css" th:href="@{/css/style.css}">
-  <script th:src="@{/js/memo.js}"></script>
-</head>
-</html>
