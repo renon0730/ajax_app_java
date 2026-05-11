c922ea74f6dcbc5dd1f8781bbd34198cf026af48
renon0730 <r.inoue@ibis-japan.co.jp>
2026-05-11 11:17:57 +0900

    メモ投稿機能

---
+++ src/main/resources/static/js/memo.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org" lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>AjaxApp</title>
+  <link rel="stylesheet" type="text/css" th:href="@{/css/style.css}">
+  <script th:src="@{/js/memo.js}"></script>
+</head>
+</html>
